569faf4be81ee56dfdb2076b75165f1a800a73a5
Adriana Werlick <isabelwerlick@gmail.com>
2026-03-14 20:21:49 -0300

    Update dart_cuso_dois.dart

--- bin/dart_cuso_dois.dart
+++ bin/dart_cuso_dois.dart
@@ -1,3 +1,10 @@
 void main() {
- print("Oi mundo!!"); 
+  String nome = "Laranja";
+  double peso = 100.2;
+  String cor = "Verde e Amarela";
+  String sabor = "Doce e cítrica";
+  int diasDesdeColheita = 40;
+  bool istaMadura;
 }
+
+//https://cursos.alura.com.br/course/dart-entendendo-orientacao-objetos/task/104856//
