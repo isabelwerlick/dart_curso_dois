36e6d7c137a5d1a324ee7b78d0d6cf9e8d4e6cbd
Adriana Werlick <isabelwerlick@gmail.com>
2026-03-14 20:57:33 -0300

    Update dart_cuso_dois.dart

--- bin/dart_cuso_dois.dart
+++ bin/dart_cuso_dois.dart
@@ -1,14 +1,14 @@
-void main() {      // é a função principal do programa, onde a execução começa//
-  String nome = "Laranja";      // variável do tipo String que armazena o nome da fruta//  //texto//
-  double peso = 100.2;          // variável do tipo double que armazena o peso da fruta//   //número com casas decimais//
-  String cor = "Verde e Amarela";       // variável do tipo String que armazena a cor da fruta//  //texto//
-  String sabor = "Doce e cítrica";      // variável do tipo String que armazena o sabor da fruta//  //texto//
-  int diasDesdeColheita = 40;            // variável do tipo int que armazena o número de dias desde a colheita da fruta//   //número inteiro//
-  bool istaMadura;                       // variável do tipo bool que indica se a fruta está madura ou não//  //verdadeiro ou falso//
+void main() {                                                                    // é a função principal do programa, onde a execução começa//
+  String nome = "Laranja";                                                       // variável do tipo String que armazena o nome da fruta//  //texto//
+  double peso = 100.2;                                                           // variável do tipo double que armazena o peso da fruta//   //número com casas decimais//
+  String cor = "Verde e Amarela";                                                // variável do tipo String que armazena a cor da fruta//  //texto//
+  String sabor = "Doce e cítrica";                                               // variável do tipo String que armazena o sabor da fruta//  //texto//
+  int diasDesdeColheita = 40;                                                    // variável do tipo int que armazena o número de dias desde a colheita da fruta//   //número inteiro//
+  bool istaMadura;                                                               // variável do tipo bool que indica se a fruta está madura ou não//  //verdadeiro ou falso//
 
-  if (diasDesdeColheita >= 30) {          // condição que verifica se a fruta está madura com base no número de dias desde a colheita//
-    istaMadura = true;                   // se a condição for verdadeira, a variável istaMadura é definida como true (verdadeiro)//
-  } else {                               // se a condição for falsa, a variável istaMadura é definida como false (falso)//
+  if (diasDesdeColheita >= 30) {                                                 // condição que verifica se a fruta está madura com base no número de dias desde a colheita//
+    istaMadura = true;                                                           // se a condição for verdadeira, a variável istaMadura é definida como true (verdadeiro)//
+  } else {                                                                       // se a condição for falsa, a variável istaMadura é definida como false (falso)//
     istaMadura = false;                   }
 }
 
