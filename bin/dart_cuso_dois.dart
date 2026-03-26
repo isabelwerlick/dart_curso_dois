7b77cb72b00c4ffa1e0a7d773bd1a32b82689c97
Adriana Werlick <isabelwerlick@gmail.com>
2026-03-26 16:41:55 -0300

    Update dart_cuso_dois.dart

--- bin/dart_cuso_dois.dart
+++ bin/dart_cuso_dois.dart
@@ -1,7 +1,5 @@
 import 'dart:ffi';
-
-void main() {
-  
+void main() {  
   String nome ="Laranja"; 
   double peso = 100.2; 
   String cor = "Verde e Amarela"; 
@@ -9,7 +7,37 @@ void main() {
   int diasDesdeColheita = 40; 
   bool isMadura = funcEstaMadura(diasDesdeColheita); 
   mostrarMadura("Uva", 40, cor: "Roxa");
+  funcQuantosDiasMadura(diasDesdeColheita);
+  Fruta fruta01 = Fruta("Uva", 150.0, "Roxa", "Doce", 40, isMadura: isMadura);
+  Fruta fruta02 = Fruta("Laranja", 100.2, "Verde e Amarela", "Doce e cítrica", 40, isMadura: isMadura); 
+
+  print(fruta01.nome);
+  print(fruta01.peso);
+  print(fruta02.nome);
+  print(fruta02.peso);
+
+
   }
+  class Fruta{
+  String nome;
+  double peso;
+  String cor;
+  String sabor;
+  int diasDesdeColheita;
+  bool? isMadura; 
+
+  Fruta(this.nome, this.peso, this.cor, this.sabor, this.diasDesdeColheita, {this.isMadura});     
+  }
+  
+  
+funcQuantosDiasMadura(int dias) {
+  int diasParaMadura = 30 - dias; 
+  if (diasParaMadura > 0) {
+    print("Faltam $diasParaMadura dias para a fruta amadurecer."); 
+  } else {
+    print("A fruta já está madura."); 
+  }
+}
 
 mostrarMadura(String nome, int dias, {required String cor}) {
   
